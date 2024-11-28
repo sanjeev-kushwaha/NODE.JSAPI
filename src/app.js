4f221f3b60ed5643db35cec4aecb64d666a91217
sanjeev-kushwaha <134046696+sanjeev-kushwaha@users.noreply.github.com>
2024-11-28 11:10:17 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const config = require("./config/config");
 const jwt = require("jsonwebtoken");
 
 const app = express();
-const port = process.env.port || 5000;
+const port = 5000;
 
 
 //================ ===> All Authentication  code like user Registration and User Login with JWT Token are Start  👇👇👇/////////////
