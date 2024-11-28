31d50475a7fcd85acedbb03f261603cf470fa8ba
sanjeev-kushwaha <134046696+sanjeev-kushwaha@users.noreply.github.com>
2024-11-28 11:31:45 +0530

    Update connection.js

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // HERE THE DATABASE NAME IS (UserMessage)
 
-mongoose.connect("mongodb://localhost:27017/RegistrationDetails",{
+mongoose.connect("mongodb+srv://sanjeevkush06:<db_password>@dbs24.uqlvy.mongodb.net/?retryWrites=true&w=majority&appName=dbs24",{
     useUnifiedTopology:true,
     useNewUrlParser:true
   }).then(() => {
